0b57d7eb43268aea1b468e31ce85471e76898643
cocodori <moradulee@gmail.com>
2021-06-03 22:24:47 +0900

    BOJ 1924

--- src/main/kotlin/boj/Boj1924.kt
+++ src/main/kotlin/boj/Boj1924.kt
@@ -1,5 +1,7 @@
 package boj
 
+import java.util.*
+
 /**
  * 문제
 오늘은 2007년 1월 1일 월요일이다. 그렇다면 2007년 x월 y일은 무슨 요일일까? 이를 알아내는 프로그램을 작성하시오.
@@ -10,7 +12,34 @@ package boj
 출력
 첫째 줄에 x월 y일이 무슨 요일인지에 따라 SUN, MON, TUE, WED, THU, FRI, SAT중 하나를 출력한다.
  */
-
 fun main() {
+    val sc = Scanner(System.`in`)
+    val month = sc.nextInt()
+    val day = sc.nextInt()
 
+    var sumDay = day
+
+    for (i in 0 until month-1)
+        sumDay += Months.values()[i].day
+
+    println(DayOfWeek.values()[sumDay % 7])
+}
+
+enum class DayOfWeek {
+    SUN, MON, TUE, WED, THU, FRI, SAT
+}
+
+enum class Months(val day: Int) {
+    JANUARY(31),
+    FEBRUARY(28),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DECEMBER(31)
 }
